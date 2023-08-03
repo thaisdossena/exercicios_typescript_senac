8af15b7915e68d6bd5fe5ce88af7892407c16729
Thaís Dossena <thaisdossena@gmail.com>
2023-08-03 09:23:12 -0300

    Delete livro_002 directory

--- livro_002/livro.ts
+++
@@ -1,3 +0,0 @@
-// Segunda Questão Crie uma classe chamada "Livro" com as propriedades título, autor e anoPublicacao. Adicione um método chamado "obterDetalhes" que retorna uma string com todas as informações do livro. Em seguida, crie uma instância da classe e chame o método "obterDetalhes".
-
-C
--- livro_002/livroTeste.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var livro_1 = require("./livro");
-var rl = require("readline-sync");
-var titulo = rl.question("Qual o titulo do livro? ");
-var autor = rl.question("De qual autor? ");
-var anoPublicacao = rl.question("Data de lançamento: ");
-var imprimirDatalhes = new livro_1.Livro(titulo, autor, anoPublicacao);
-imprimirDatalhes.obterDetalhes();
